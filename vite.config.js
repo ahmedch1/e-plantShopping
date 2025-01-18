5b004f2fae920bd6454dc5537bc7f60f79c8f6c0
Ahmed Chouihi <ahmedchouihi@gmail.com>
2025-01-18 11:49:32 +0100

    adjust base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "https://darling-frangipane-290669.netlify.app/",
   plugins: [react()],
 })
